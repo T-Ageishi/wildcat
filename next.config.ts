ea2fcadc2935c5f7d5c0beb030ade97e045f6f75
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-01-18 11:11:48 +0900

    [Chore]: Install Sass

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	sassOptions: {
+		variables: `@import @/styles/_variables.scss`,
+	},
 };
 
 export default nextConfig;
