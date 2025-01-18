8f3e5f38e0026aa32416c7b73eee6e25ee792b3f
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-01-18 20:23:43 +0900

    [Feat]: Add NewProjectCard component to projects page

--- src/components/pages/projects/index.tsx
+++ src/components/pages/projects/index.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/organisms/header";
 import styles from "./index.module.css";
+import NewProjectCard from "@/components/molecules/new-project-card";
 
 export default function ListPage({}: ListPageProps) {
 	return (
 		<div className={styles["page"]}>
 			<Header />
 			<main className={styles["page__body"]}>
-				<p>プロジェクト一覧のページです。</p>
+				<section className={styles["page__cards"]}>
+					<NewProjectCard />
+				</section>
 			</main>
 		</div>
 	);
