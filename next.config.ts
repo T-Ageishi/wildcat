9b2e33204cf1eef0a2f7a167f6e29627b165dd81
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-01-18 12:15:32 +0900

    [Chore]: Uninstall Sass and use css/css modules

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	sassOptions: {
-		variables: `@import @/styles/_variables.scss`,
-	},
 };
 
 export default nextConfig;
