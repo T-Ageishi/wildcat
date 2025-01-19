b774abf8eaa5419e44b26d879d7981630ee86fdb
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-01-20 07:53:39 +0900

    [Feat]: Add Text component in projects page

--- src/components/organisms/projects-search/index.tsx
+++ src/components/organisms/projects-search/index.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { ChangeEventHandler, useState } from "react";
+import Text from "@/components/atoms/text";
+
 export default function ProjectsSearch({}: ProjectsSearchProps) {
+	const [keyword, setKeyword] = useState<string>("");
+	const onChangeText: ChangeEventHandler<HTMLInputElement> = (e) => {
+		setKeyword(e.target.value);
+	};
+
 	return (
 		<section>
-			<div>キーワード検索</div>
+			<div>
+				<Text value={keyword} onChange={onChangeText} prependIcon={"search"} />
+			</div>
 			<div>タグでの絞り込み</div>
 		</section>
 	);
