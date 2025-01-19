1429770732834f51168ed57e72e536ec7e4164e0
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-01-19 11:08:33 +0900

    [Feat]: Modify projects page
    - Add a hero

--- src/components/pages/projects/index.tsx
+++ src/components/pages/projects/index.tsx
@@ -2,18 +2,22 @@ import Header from "@/components/organisms/header";
 import NewProjectCard from "@/components/molecules/new-project-card";
 import Footer from "@/components/molecules/footer";
 import styles from "./index.module.css";
+import Hero from "@/components/atoms/hero";
 
 export default function ListPage({}: ListPageProps) {
 	return (
 		<div className={styles["page"]}>
 			<Header />
 			<main className={styles["main"]}>
-				<section>
-					<div>キーワード検索</div>
-					<div>タグでの絞り込み</div>
-				</section>
-				<section className={styles["page__cards"]}>
-					<NewProjectCard />
+				<Hero label={"プロジェクト一覧"} />
+				<section className={styles["main__contents"]}>
+					<section>
+						<div>キーワード検索</div>
+						<div>タグでの絞り込み</div>
+					</section>
+					<section className={styles["page__cards"]}>
+						<NewProjectCard />
+					</section>
 				</section>
 			</main>
 			<div className={styles["footer"]}>
