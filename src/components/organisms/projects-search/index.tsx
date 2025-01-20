3ff595db4200f24fc604798cf5e66231cfd752a1
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-01-20 13:08:41 +0900

    [Feat]: Add chips in projects page

--- src/components/organisms/projects-search/index.tsx
+++ src/components/organisms/projects-search/index.tsx
@@ -2,19 +2,39 @@
 
 import { ChangeEventHandler, useState } from "react";
 import Text from "@/components/atoms/text";
+import Chips, { ChipProps } from "@/components/atoms/chips";
+import styles from "./index.module.css";
 
 export default function ProjectsSearch({}: ProjectsSearchProps) {
 	const [keyword, setKeyword] = useState<string>("");
+	const [selectedChips, setSelectedChips] = useState<number[]>([]);
+
 	const onChangeText: ChangeEventHandler<HTMLInputElement> = (e) => {
 		setKeyword(e.target.value);
 	};
+	const onChangeChips: ChangeEventHandler<HTMLInputElement> = (e) => {
+		const value = Number(e.currentTarget.value);
+		if (e.currentTarget.checked) {
+			setSelectedChips([...selectedChips, value]);
+		} else {
+			setSelectedChips(selectedChips.filter((v) => v !== value));
+		}
+	};
+	const props: ChipProps[] = [
+		{ label: "その他", value: -99999, onChange: onChangeChips },
+	];
 
 	return (
 		<section>
-			<div>
-				<Text value={keyword} onChange={onChangeText} prependIcon={"search"} />
+			<Text
+				value={keyword}
+				onChange={onChangeText}
+				prependIcon={"search"}
+				placeholder={"プロジェクトを検索"}
+			/>
+			<div className={styles["chips-container"]}>
+				<Chips chipPropsList={props} />
 			</div>
-			<div>タグでの絞り込み</div>
 		</section>
 	);
 }
